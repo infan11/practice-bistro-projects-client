49647049610c019ef0a0ba597744a90127d2c2fd
infan11 <infanjiounrahman20606@gmail.com>
2024-10-17 19:20:11 +0600

    added .env.local

--- src/Componenets/Firebase/firebase.config.js
+++ src/Componenets/Firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAAO7Ws91ThQOu6Ig4TQxpH0EXYkUlyFG0",
-  authDomain: "practice-bistro-boss-76004.firebaseapp.com",
-  projectId: "practice-bistro-boss-76004",
-  storageBucket: "practice-bistro-boss-76004.appspot.com",
-  messagingSenderId: "276896028259",
-  appId: "1:276896028259:web:85d0b1b451d1627ea24a56"
+  apiKey: meta.env.VITE_apiKey,
+  authDomain: meta.env.VITE_authDomain,
+  projectId: meta.env.VITE_projectId,
+  storageBucket: meta.env.VITE_storageBucket,
+  messagingSenderId: meta.env.VITE_messagingSenderIdy,
+  appId: meta.env.VITE_appId
 };
 
 // Initialize Firebase
